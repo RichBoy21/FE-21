140d82f56f2b2e5b61d9563a262ba22ee2be7520
RichBoy21 <frommrinat@gmail.com>
2023-02-28 22:13:46 +0100

    JS_1

---
+++ JSDZ_28.02/script.js
@@ -0,0 +1,31 @@
+//const number = prompt("chislo")
+//console.log(number / 100 * 10)
+
+
+/*const number1 = prompt("chislo")
+const number2 = prompt("chislo")
+if (number1<number2) {
+    console.log("number1")
+} else  {
+    console.log("number2") }
+    */
+   
+
+
+/*const number1 = prompt("vedite chislo")
+if (number1 < 100) {
+    console.log ("number1")
+}else if (number1 > 100) {
+    console.log ("number2")
+}else (number1 == 100) (
+    console.log ("number3")
+)*/
+
+
+/*const name = prompt ("your name")
+const age = prompt ("your age")
+if (age > 18) {
+    console.log ("Hello" + name)
+}else if (age < 18) (
+    console.log ("Hi" + name)
+)*/
