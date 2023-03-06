ca01c3af39aa9af946f650c19f3b00dde6662036
RichBoy21 <frommrinat@gmail.com>
2023-03-06 18:49:53 +0100

    DZ02

---
+++ JS_DZ02/script.js
@@ -0,0 +1,38 @@
+/*for (let i = 0; i <= 10; i += 2) {
+    console.log(i);
+}
+
+
+for (let i = 55; i >= 40; i --) {/
+    console.log(i);
+}*/
+
+
+/*const numbers = [3, 5, 11, 2, 8, 1,6]
+console.log(numbers)*/
+
+/*const number = [3, 5, 11, 2, 8, 1,6]
+
+const number_squered = []
+
+for (let i = 0; i < number.length; i++){
+    console.log (number[i]*number[i]);
+}
+ const last_elem = number_squered[number_squered.length - 1];
+   console.log (last_elem);+/*/
+
+/*const user = {
+    first_name: 'Ivanov',
+    last_name: 'Ivanov',
+    age: 20,
+    salary: 500
+}
+
+console.log (`User’s name is ${user.first_name} ${user.last_name} He is ${user.age} years old`)*/
+
+
+ 
+  
+
+
+
